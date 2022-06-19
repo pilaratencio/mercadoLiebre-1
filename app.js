86ffc78552d28c4738bfb0bfb69e32eee37adffb
pilaratencio <pilaratencio@gmail.com>
2022-06-19 20:44:14 -0300

    formularios

--- app.js
+++ app.js
@@ -6,9 +6,17 @@ const { send } = require("process");
 const publicPath = path.resolve(__dirname,'./public');
 app.use(express.static(publicPath));
 
-app.listen('4000', ()=>console.log('Servidor corriendo en puerto 4000'))
-
 app.get('/',(req,res)=>res.sendFile(path.resolve(__dirname,'./views/home.html')))
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + "/views/login.html");
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+app.listen('4000', ()=>console.log('Servidor corriendo en puerto 4000'))
 
 
 
